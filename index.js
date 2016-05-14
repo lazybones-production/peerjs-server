0547622beaa456ccf6cc5ed8df9f7970134483cc
Sergey Zelenov <szelenov@spbtv.com>
2016-05-14 21:53:51 +0300

    try to fix crossdomain error

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-var PeerServer = require('peer').PeerServer,
+var ExpressPeerServer = require('peer').ExpressPeerServer,
   express = require('express'),
   app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server),
+
   port = process.env.PORT || 3001;
-
-var expressServer = app.listen(port);
-var io = require('socket.io').listen(expressServer);
-
-var peerServer = new PeerServer({ port: 9000, path: '/peer' });
 
 var USERS_LIST_EVENT = 'USERS_LIST_EVENT';
 var connectedUsers = [];
 
-peerServer.on('connection', function (id) {
+app.get('/', function(req, res, next) { res.send('Hello world!'); });
+
+app.use('/peerjs', ExpressPeerServer(server, {port: 9000}));
+
+server.on('connection', function (id) {
   connectedUsers = connectedUsers.concat(id);
   io.emit(USERS_LIST_EVENT, connectedUsers);
   console.log('User connected with #', id);
 });
 
-peerServer.on('disconnect', function (id) {
+server.on('disconnect', function (id) {
   connectedUsers = connectedUsers.filter(_id => _id !== id);
   io.emit(USERS_LIST_EVENT, connectedUsers);
   console.log('User disconnected with #', id);
 });
+
+server.listen(port);
